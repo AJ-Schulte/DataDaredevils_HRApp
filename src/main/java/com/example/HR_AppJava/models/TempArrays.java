98b8ad529dbe661720f28d8a8d2f46e5971f56d4
Ethan-Standridge <ethan.standridge02@yahoo.com>
2024-03-29 13:27:01 -0500

    Demographics methods

--- src/main/java/com/example/HR_AppJava/models/TempArrays.java
+++ src/main/java/com/example/HR_AppJava/models/TempArrays.java
@@ -1,8 +1,24 @@
 package com.example.HR_AppJava.models;
-
+import java.util.ArrayList;
 public class TempArrays {
     //User array (Noah?)
+
     //EmployeeDemographics array (Ethan)
+    static ArrayList<Object> employeeDemographicsArray = new ArrayList<>();
+    public void addDemographic(Object e) {
+        employeeDemographicsArray.add(e);
+    }
+    public void removeDemographic(Object e) {
+        employeeDemographicsArray.remove(e);
+    }
+    public Object getDemographic(int i){
+        return employeeDemographicsArray.get(i);
+    }
+    public void setDemographic(int i, Object e){
+        employeeDemographicsArray.set(i,e);
+    }
+
     //EmployeeJobHistory (AJ)
+
     //EmployeeEvaluation (Noah)
 }
