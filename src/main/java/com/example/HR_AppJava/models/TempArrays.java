f9128d68a2fa1ae1657ab42290705e929c00b0f3
Ethan-Standridge <ethan.standridge02@yahoo.com>
2024-03-29 12:44:36 -0500

    pseudocode for temparrays

---
+++ src/main/java/com/example/HR_AppJava/models/TempArrays.java
@@ -0,0 +1,8 @@
+package com.example.HR_AppJava.models;
+
+public class TempArrays {
+    //User array (Noah?)
+    //EmployeeDemographics array (Ethan)
+    //EmployeeJobHistory (AJ)
+    //EmployeeEvaluation (Noah)
+}
