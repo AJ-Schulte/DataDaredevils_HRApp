2f808837930d1d63f9c702594ddb82046105b15e
Ethan-Standridge <ethan.standridge02@yahoo.com>
2024-04-03 13:13:50 -0500

    Conflict resolution

--- src/main/java/com/example/HR_AppJava/models/TempArrays.java
+++ src/main/java/com/example/HR_AppJava/models/TempArrays.java
@@ -1,7 +1,26 @@
 package com.example.HR_AppJava.models;
+
 import java.util.ArrayList;
+
 public class TempArrays {
-    //User array (Noah?)
+    // User array (AJ)
+    private ArrayList<Object> userArray = new ArrayList<>();
+
+    public void addUser(User user) {
+        userArray.add(user);
+    }
+
+    public void addUserAt(int index, User user) {
+        userArray.add(index, user);
+    }
+
+    public void removeUser(User user) {
+        userArray.remove(user);
+    }
+
+    public Object getUser(int index) {
+        return userArray.get(index);
+    }
 
     //EmployeeDemographics array (Ethan)
     static ArrayList<Object> employeeDemographicsArray = new ArrayList<>();
@@ -19,6 +38,22 @@ public class TempArrays {
     }
 
     //EmployeeJobHistory (AJ)
+    private ArrayList<Object> jobHistory = new ArrayList<>();
 
+    public void addToJobHistory(EmployeeJobHistory job) {
+        jobHistory.add(job);
+    }
+
+    public void addJobHistoryAt(int index, EmployeeJobHistory job) {
+        jobHistory.add(index, job);
+    }
+
+    public void removeFromJobHistory(EmployeeJobHistory job) {
+        jobHistory.remove(job);
+    }
+
+    public Object getFromJobHistory(int index) {
+        return jobHistory.get(index);
+    }
     //EmployeeEvaluation (Noah)
 }
